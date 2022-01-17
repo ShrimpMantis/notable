5daae4647ea4f5d7d6c7363189c25a2f36fe48da
ShrimpMantis <jmahanta001@gmail.com>
2022-01-17 00:46:11 -0800

    checking in changes that were overdue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,23 +11,27 @@ export class AppComponent implements OnInit {
   physicians!:Array<any>;
   appointments!:Array<any>;
   currentPhysicianName!:string;
-  currentPhysicianLastName!:string
+  currentPhysicianLastName!:string;
+  chosenIndex!:number;
   constructor(private service: NotableService){
 
   }
   ngOnInit(): void {
     this.service.getPhysicians().subscribe((x)=>{
       this.physicians=x;
+      this.physicianClicked(this.physicians[0].physicianId)
     });
-
+    
   }
 
   physicianClicked(phId:string){
-    const currPhysician= this.physicians.filter((element, index)=>{
-        return element.physicianId=== phId;
-      })[0];
-      this.currentPhysicianName= currPhysician.name;
-      this.currentPhysicianLastName= currPhysician.lastName;
+    this.chosenIndex= this.physicians.findIndex((element, index)=>{
+         return element.physicianId ===phId;
+      });
+  
+      this.currentPhysicianName= this.physicians[this.chosenIndex].name;
+      this.currentPhysicianLastName= this.physicians[this.chosenIndex].lastName;
+
       this.service.getAppointments(phId).subscribe((x)=>{
           this.appointments= x;
       });
--- src/app/services/notable.service.ts
+++ src/app/services/notable.service.ts
@@ -10,13 +10,25 @@ export class NotableService {
   constructor(private httpClient: HttpClient) { }
 
   getPhysicians():Observable<any>{
-     const url="http://localhost:8080/api/physicianApi/physicians";
+    const  headers= new HttpHeaders()
+    headers.append("Access-Control-Allow-Origin","http://localhost:4200");
+    headers.append("Access-Control-Allow-Methods","Get, POST")
+    const options={
+      headers: headers
+    }
+     const url="http://localhost:5001/api/physician/physicians";
     
-      return this.httpClient.get(url);
+      return this.httpClient.get(url, options);
   }
 
   getAppointments(physicianId:string):Observable<any>{
-    const url=`http://localhost:8080/api/physicianApi/appointments/${physicianId}`;
-    return this.httpClient.get(url);
+    const url=`http://localhost:5001/api/physician/appointments/${physicianId}`;
+    const  headers= new HttpHeaders()
+    headers.append("Access-Control-Allow-Origin","http://localhost:4200");
+    headers.append("Access-Control-Allow-Methods","Get, POST")
+    const options={
+      headers: headers
+    }
+    return this.httpClient.get(url, options);
   }
 }
